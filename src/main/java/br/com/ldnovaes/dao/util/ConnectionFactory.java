3612d423baa1bcea1a8630d0ede98f92eaf65863
Ewerton Mendonça <netrometro@gmail.com>
2024-09-18 22:30:21 -0300

    Variáveis de ambiente para conexão com o banco

--- src/main/java/br/com/ldnovaes/dao/util/ConnectionFactory.java
+++ src/main/java/br/com/ldnovaes/dao/util/ConnectionFactory.java
@@ -3,6 +3,7 @@ package br.com.ldnovaes.dao.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,6 +19,18 @@ public class ConnectionFactory {
 	}
 	
 	private static Connection iniciarConexao() {
+		Map<String, String> env = System.getenv();
+		
+		for (Map.Entry<String,String> pair : env.entrySet()) {
+		    System.out.println(pair.getKey());
+		    System.out.println(pair.getValue());
+		}
+		
+		String DB_USER = env.get("DB_USER");
+		String DB_PASS = env.get("DB_PASS");
+		String DB_HOST = env.get("DB_HOST");
+		String DB_NAME = env.get("DB_NAME");
+		
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException c) {
@@ -26,7 +39,7 @@ public class ConnectionFactory {
 		
 		try {
 			
-			return DriverManager.getConnection("jdbc:postgresql://postgres-compose:5432/servlet_example", "postgres", "postgres");
+			return DriverManager.getConnection("jdbc:postgresql://" + DB_HOST+ ":5432/" + DB_NAME, DB_USER, DB_PASS);
 		} catch (SQLException e ) {
 			throw new RuntimeException(e);
 		}
